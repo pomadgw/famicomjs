b05643c9f352219c2ed381f25ffe7eb994ef38bf
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-17 22:21:51 +0700

    just include cpu into main bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const canvas = document.querySelector('#canvas')
+import CPU from './6502/cpu'
 
+const canvas = document.querySelector('#canvas')
+const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
 console.log(canvas)
+console.log(cpu)
