016f9827a6f364d772afd70b33562cd36ae280a3
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-17 15:30:00 +0700

    add indirected modes

---
+++ src/6502/instructions/addressings/indexed-indirect.js
@@ -0,0 +1,11 @@
+export default function indexedIndirectMode(cpu) {
+  const tableAddress = cpu.ram[cpu.nextPC()]
+  const lo = cpu.ram[tableAddress + cpu.registers.X]
+  const hi = cpu.ram[tableAddress + cpu.registers.X + 1]
+
+  const absoluteAddress = (hi << 8) | lo
+
+  return {
+    absoluteAddress
+  }
+}
