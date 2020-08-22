86f1fcba67136249551ffecb2cc36db6c1bfe8ab
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-22 18:54:11 +0700

    cpu: modify integration test to cover most of addressing mode

--- src/6502/cpu.test.js
+++ src/6502/cpu.test.js
@@ -2,55 +2,137 @@ import CPU from './cpu'
 
 describe('CPU', () => {
   it('should be able to execute correctly (zero page)', () => {
-    // ORA $00
-    // operator: ORA, addressing mode: zero page
-    const cpu = new CPU([0x05, 0x00])
-
-    cpu.registers.A = 0x02
+    // LDA $F0
+    // operator: LDA, addressing mode: zero page
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xa5
+    cpu.ram[0x01] = 0xf0
+    cpu.ram[0xf0] = 0x79
 
     cpu.clock()
 
-    expect(cpu.registers.A).toBe(0x02 | 0x05)
+    expect(cpu.registers.A).toBe(0x79)
   })
 
   it('should be able to execute correctly (indexed zero page, register X)', () => {
-    // ORA $00,X
-    // operator: ORA, addressing mode: zero page, X
-    const cpu = new CPU([0x15, 0x00, 0x01])
+    // LDA $F0,X
+    // operator: LDA, addressing mode: zero page, X
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
 
-    cpu.registers.A = 0x02
+    cpu.ram[0x00] = 0xb5
+    cpu.ram[0x01] = 0xf0
+    cpu.ram[0xf1] = 0x79
+    cpu.registers.X = 0x01
+
+    cpu.clock()
+
+    expect(cpu.registers.A).toBe(0x79)
+  })
+
+  it('should be able to execute correctly (indexed zero page, register Y)', () => {
+    // LDX $F0,Y
+    // operator: LDX, addressing mode: zero page, Y
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+
+    cpu.ram[0x00] = 0xb6
+    cpu.ram[0x01] = 0xf0
+    cpu.ram[0xf1] = 0x79
+    cpu.registers.Y = 0x01
+
+    cpu.clock()
+
+    expect(cpu.registers.X).toBe(0x79)
+  })
+
+  it('should be able to execute correctly (absolute)', () => {
+    // LDA $1000
+    // operator: LDA, addressing mode: absolute
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xad
+    cpu.ram[0x01] = 0x00
+    cpu.ram[0x02] = 0x10
+    cpu.ram[0x1000] = 0x05
+
+    cpu.clock()
+
+    expect(cpu.registers.A).toBe(0x05)
+  })
+
+  it('should be able to execute correctly (indexed absolute, register X)', () => {
+    // LDA $1000,X
+    // operator: LDA, addressing mode: absolute, X
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xbd
+    cpu.ram[0x01] = 0x00
+    cpu.ram[0x02] = 0x10
+    cpu.ram[0x1001] = 0x05
+    cpu.registers.X = 0x01
+
+    cpu.clock()
+
+    expect(cpu.registers.A).toBe(0x05)
+  })
+
+  it('should be able to execute correctly (indexed absolute, register Y)', () => {
+    // LDA $1000,Y
+    // operator: LDA, addressing mode: absolute, Y
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xb9
+    cpu.ram[0x01] = 0x00
+    cpu.ram[0x02] = 0x10
+    cpu.ram[0x1001] = 0x05
+    cpu.registers.Y = 0x01
+
+    cpu.clock()
+
+    expect(cpu.registers.A).toBe(0x05)
+  })
+
+  it('should be able to execute correctly (immediate)', () => {
+    // LDA #$FF
+    // operator: LDA, addressing mode: immediate
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xa9
+    cpu.ram[0x01] = 0xff
+
+    cpu.clock()
+
+    expect(cpu.registers.A).toBe(0xff)
+  })
+
+  it('should be able to execute correctly (indexed indirect)', () => {
+    // LDA ($B1,X)
+    // operator: LDA, addressing mode: indexed indirect (IZX)
+    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    cpu.ram[0x00] = 0xa1
+    cpu.ram[0x01] = 0xb0
+
+    cpu.ram[0xb2] = 0x00
+    cpu.ram[0xb3] = 0x10
+
+    cpu.ram[0x1000] = 0x05
     cpu.registers.X = 0x02
 
     cpu.clock()
 
-    expect(cpu.registers.A).toBe(0x02 | 0x01)
+    expect(cpu.registers.A).toBe(0x05)
   })
 
-  it('should be able to execute correctly (absolute)', () => {
-    // ORA $1000
-    // operator: ORA, addressing mode: absolute
+  it('should be able to execute correctly (indirect indexed)', () => {
+    // LDA ($B1),Y
+    // operator: LDA, addressing mode: indirect indexed (IZY)
     const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0] = 0x0d
-    cpu.ram[1] = 0x00
-    cpu.ram[2] = 0x10
-    cpu.ram[0x1000] = 0x05
+    cpu.ram[0x00] = 0xb1
+    cpu.ram[0x01] = 0xb1
 
-    cpu.registers.A = 0x02
+    cpu.ram[0xb1] = 0x00
+    cpu.ram[0xb2] = 0x10
 
-    cpu.clock()
-
-    expect(cpu.registers.A).toBe(0x05 | 0x02)
-  })
-
-  it('should be able to execute correctly (immediate)', () => {
-    // ORA #10
-    // operator: ORA, addressing mode: immediate
-    const cpu = new CPU([0x09, 0x10])
-
-    cpu.registers.A = 0x02
+    cpu.ram[0x100f] = 0x05
+    cpu.registers.Y = 0x0f
 
     cpu.clock()
 
-    expect(cpu.registers.A).toBe(0x02 | 0x10)
+    expect(cpu.registers.A).toBe(0x5)
   })
 })
