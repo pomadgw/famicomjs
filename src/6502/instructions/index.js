51a099b8d39b3eb5f96e74d4ea3e53308017b5d7
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-22 11:54:34 +0700

    add opcode table

--- src/6502/instructions/index.js
+++ src/6502/instructions/index.js
@@ -1,29 +1,64 @@
 /* eslint-disable no-unused-vars */
-import abs from './addressings/absolute'
-import rel from './addressings/relative'
-import imm from './addressings/immediate'
-import ind from './addressings/indirect'
-import inx from './addressings/indexed-indirect'
-import iny from './addressings/indirect-indexed'
-import zpg from './addressings/zero-page'
+import ABS from './addressings/absolute'
+import REL from './addressings/relative'
+import IMM from './addressings/immediate'
+import IND from './addressings/indirect'
+import INX from './addressings/indexed-indirect'
+import INY from './addressings/indirect-indexed'
+import ZPG from './addressings/zero-page'
+import IMP from './addressings/implicit'
 
-import { ORA } from './operations/bitwise'
+import * as bitwise from './operations/bitwise'
 
-export default {
-  0x05: {
-    addressing: zpg,
-    operator: ORA
-  },
-  0x09: {
-    addressing: imm,
-    operator: ORA
-  },
-  0x0d: {
-    addressing: abs,
-    operator: ORA
-  },
-  0x15: {
-    addressing: (cpu) => zpg(cpu, 'X'),
-    operator: ORA
-  }
+import opcodes from './opcodes.json'
+
+const NOP = () => {}
+
+const addressingTable = {
+  IMM,
+  ABS,
+  ABX: (cpu) => ABS(cpu, 'X'),
+  ABY: (cpu) => ABS(cpu, 'Y'),
+  REL,
+  IND,
+  IMP,
+  ZP0: ZPG,
+  ZPX: (cpu) => ZPG(cpu, 'X'),
+  ZPY: (cpu) => ZPG(cpu, 'Y')
 }
+
+const generateOpcodeTable = () => {
+  const result = {}
+  // eslint-disable-next-line camelcase
+  opcodes.forEach(({ name, operate, addr_mode, cycles }, index) => {
+    result[index] = {
+      name,
+      operator: bitwise[operate.toUpperCase()] ?? NOP,
+      addressing: addressingTable[addr_mode],
+      cycles
+    }
+  })
+
+  return result
+}
+
+export default generateOpcodeTable()
+
+// export default {
+//   0x05: {
+//     addressing: zpg,
+//     operator: ORA
+//   },
+//   0x09: {
+//     addressing: imm,
+//     operator: ORA
+//   },
+//   0x0d: {
+//     addressing: abs,
+//     operator: ORA
+//   },
+//   0x15: {
+//     addressing: (cpu) => zpg(cpu, 'X'),
+//     operator: ORA
+//   }
+// }
