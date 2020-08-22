134413e66362d2c85dbdcfcd8ef528ab5dbb1e8c
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-22 17:45:24 +0700

    add stack instructions

--- src/6502/instructions/operations/stack.js
+++ src/6502/instructions/operations/stack.js
@@ -1,3 +1,5 @@
+import { FLAGS } from '../../register'
+
 export function PHA(cpu) {
   cpu.pushStack(cpu.registers.A)
   return 0
@@ -7,5 +9,34 @@ export function PLA(cpu) {
   cpu.registers.A = cpu.popStack()
   cpu.registers.STATUS.N = cpu.registers.A > 0x80
   cpu.registers.STATUS.Z = cpu.registers.A === 0
+  return 0
+}
+
+export function PHP(cpu) {
+  const statusToBePushed = +cpu.registers.STATUS | FLAGS.U | FLAGS.B
+
+  cpu.registers.STATUS.U = false
+  cpu.registers.STATUS.B = false
+
+  cpu.pushStack(statusToBePushed)
+  return 0
+}
+
+export function PLP(cpu) {
+  const status = cpu.popStack()
+  cpu.registers.STATUS.status = status
+  cpu.registers.STATUS.U = true
+  return 0
+}
+
+export function TXS(cpu) {
+  cpu.registers.SP = cpu.registers.X
+  return 0
+}
+
+export function TSX(cpu) {
+  cpu.registers.X = cpu.registers.SP
+  cpu.registers.STATUS.N = cpu.registers.X > 0x80
+  cpu.registers.STATUS.Z = cpu.registers.X === 0
   return 0
 }
--- src/6502/instructions/operations/stack.test.js
+++ src/6502/instructions/operations/stack.test.js
@@ -61,4 +61,77 @@ describe('instructions: stack operations', () => {
       expect(cpu.registers.STATUS.Z).toBe(false)
     })
   })
+
+  describe('PHP', () => {
+    it('should push status register value into stack and set correct flags appropriately', () => {
+      const cpu = new CPU(generateArray(0x200))
+      const status = 0b11000001
+      cpu.registers.STATUS.status = status
+      cpu.fetch()
+      const oldStackPointer = cpu.registers.SP
+
+      stack.PHP(cpu)
+      expect(cpu.registers.SP).toBe(oldStackPointer - 1)
+      expect(cpu.ram[oldStackPointer + 0x100]).toBe(status | 0b00110000)
+      expect(cpu.registers.STATUS.U).toBe(false)
+      expect(cpu.registers.STATUS.B).toBe(false)
+    })
+  })
+
+  describe('PLP', () => {
+    it('should pop into status register and set correct flags appropriately', () => {
+      const cpu = new CPU(generateArray(0x200))
+      cpu.fetch()
+
+      const status = 0b11110001
+      const oldStackPointer = 0xfe
+      cpu.ram[oldStackPointer + 0x100] = status
+      cpu.registers.SP = oldStackPointer
+
+      stack.PLP(cpu)
+      expect(cpu.registers.SP).toBe(oldStackPointer + 1)
+      expect(+cpu.registers.STATUS).toBe(status)
+      expect(cpu.registers.STATUS.U).toBe(true)
+    })
+  })
+
+  describe('TSX', () => {
+    it('should transfer stack pointer register to register X', () => {
+      const cpu = new CPU(generateArray(0x200))
+      cpu.fetch()
+
+      cpu.registers.SP = 0xff
+      stack.TSX(cpu)
+      expect(cpu.registers.X).toBe(0xff)
+    })
+
+    it('should toggle N flag correctly', () => {
+      const cpu = new CPU(generateArray(0x200))
+      cpu.fetch()
+
+      cpu.registers.SP = 0xff
+      stack.TSX(cpu)
+      expect(cpu.registers.STATUS.N).toBe(true)
+    })
+
+    it('should toggle Z flag correctly', () => {
+      const cpu = new CPU(generateArray(0x200))
+      cpu.fetch()
+
+      cpu.registers.SP = 0x00
+      stack.TSX(cpu)
+      expect(cpu.registers.STATUS.Z).toBe(true)
+    })
+  })
+
+  describe('TXS', () => {
+    it('should transfer register X to stack pointer register', () => {
+      const cpu = new CPU(generateArray(0x200))
+      cpu.fetch()
+
+      cpu.registers.X = 0xe0
+      stack.TXS(cpu)
+      expect(cpu.registers.SP).toBe(0xe0)
+    })
+  })
 })
