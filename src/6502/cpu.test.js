4702290bdee40fb6781d41936cb040363f90c224
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-24 19:36:23 +0700

    use assembler for integration test

--- src/6502/cpu.test.js
+++ src/6502/cpu.test.js
@@ -1,12 +1,16 @@
 import CPU from './cpu'
+import assemblerGenerator from './assembler'
 
 describe('CPU', () => {
+  const a6502 = assemblerGenerator({ memorySize: 0x2000, PC: 0 })
+
   it('should be able to execute correctly (zero page)', () => {
     // LDA $F0
     // operator: LDA, addressing mode: zero page
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xa5
-    cpu.ram[0x01] = 0xf0
+    // const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    const cpu = new CPU(a6502`LDA $F0`)
+    // cpu.ram[0x00] = 0xa5
+    // cpu.ram[0x01] = 0xf0
     cpu.ram[0xf0] = 0x79
 
     cpu.clock()
@@ -17,10 +21,8 @@ describe('CPU', () => {
   it('should be able to execute correctly (indexed zero page, register X)', () => {
     // LDA $F0,X
     // operator: LDA, addressing mode: zero page, X
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    const cpu = new CPU(a6502`LDA $F0,X`)
 
-    cpu.ram[0x00] = 0xb5
-    cpu.ram[0x01] = 0xf0
     cpu.ram[0xf1] = 0x79
     cpu.registers.X = 0x01
 
@@ -32,10 +34,8 @@ describe('CPU', () => {
   it('should be able to execute correctly (indexed zero page, register Y)', () => {
     // LDX $F0,Y
     // operator: LDX, addressing mode: zero page, Y
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
+    const cpu = new CPU(a6502`LDX $F0,Y`)
 
-    cpu.ram[0x00] = 0xb6
-    cpu.ram[0x01] = 0xf0
     cpu.ram[0xf1] = 0x79
     cpu.registers.Y = 0x01
 
@@ -47,10 +47,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (absolute)', () => {
     // LDA $1000
     // operator: LDA, addressing mode: absolute
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xad
-    cpu.ram[0x01] = 0x00
-    cpu.ram[0x02] = 0x10
+    const cpu = new CPU(a6502`LDA $1000`)
     cpu.ram[0x1000] = 0x05
 
     cpu.clock()
@@ -61,10 +58,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (indexed absolute, register X)', () => {
     // LDA $1000,X
     // operator: LDA, addressing mode: absolute, X
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xbd
-    cpu.ram[0x01] = 0x00
-    cpu.ram[0x02] = 0x10
+    const cpu = new CPU(a6502`LDA $1000,X`)
     cpu.ram[0x1001] = 0x05
     cpu.registers.X = 0x01
 
@@ -76,10 +70,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (indexed absolute, register Y)', () => {
     // LDA $1000,Y
     // operator: LDA, addressing mode: absolute, Y
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xb9
-    cpu.ram[0x01] = 0x00
-    cpu.ram[0x02] = 0x10
+    const cpu = new CPU(a6502`LDA $1000,Y`)
     cpu.ram[0x1001] = 0x05
     cpu.registers.Y = 0x01
 
@@ -91,9 +82,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (immediate)', () => {
     // LDA #$FF
     // operator: LDA, addressing mode: immediate
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xa9
-    cpu.ram[0x01] = 0xff
+    const cpu = new CPU(a6502`LDA #$FF`)
 
     cpu.clock()
 
@@ -101,11 +90,9 @@ describe('CPU', () => {
   })
 
   it('should be able to execute correctly (indexed indirect)', () => {
-    // LDA ($B1,X)
+    // LDA ($B0,X)
     // operator: LDA, addressing mode: indexed indirect (IZX)
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xa1
-    cpu.ram[0x01] = 0xb0
+    const cpu = new CPU(a6502`LDA ($B0,X)`)
 
     cpu.ram[0xb2] = 0x00
     cpu.ram[0xb3] = 0x10
@@ -121,9 +108,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (indirect indexed)', () => {
     // LDA ($B1),Y
     // operator: LDA, addressing mode: indirect indexed (IZY)
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0xb1
-    cpu.ram[0x01] = 0xb1
+    const cpu = new CPU(a6502`LDA ($B1),Y`)
 
     cpu.ram[0xb1] = 0x00
     cpu.ram[0xb2] = 0x10
@@ -139,10 +124,7 @@ describe('CPU', () => {
   it('should be able to execute correctly (indirect)', () => {
     // JMP ($1234)
     // operator: JMP, addressing mode: indirect
-    const cpu = new CPU([...new Array(0x2000)].map((_) => 0))
-    cpu.ram[0x00] = 0x6c
-    cpu.ram[0x01] = 0x34
-    cpu.ram[0x02] = 0x12
+    const cpu = new CPU(a6502`JMP ($1234)`)
 
     cpu.ram[0x1234] = 0x00
     cpu.ram[0x1235] = 0x10
