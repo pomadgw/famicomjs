43d391a9a6becd3fafe43d1ea35023f60c93c539
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-29 21:29:15 +0700

    hijack cpu ram

--- src/bus/index.js
+++ src/bus/index.js
@@ -3,5 +3,40 @@ export default class Bus {
     this.cpu = cpu
 
     this.cpu.connect(this)
+
+    this.initVRAM()
+  }
+
+  initVRAM() {
+    const cpuRAM = new Uint8Array(0x2000)
+
+    const proxyRAM = new Proxy(cpuRAM, {
+      get(target, prop) {
+        if (isNaN(Number(prop))) return target[prop]
+
+        // TODO:
+        // Add routine for fetching data from cartridge
+        // eslint-disable-next-line no-constant-condition
+        if (false) return 0
+        else if (prop < 0x2000) return target[prop & 0x07ff]
+        else if (prop < 0x4000) return 0 // here return content of PPU
+        return 0
+      },
+      set(target, prop, value) {
+        if (isNaN(Number(prop))) target[prop] = value
+        // TODO:
+        // Add routine for fetching data writing status from cartridge
+        // eslint-disable-next-line no-constant-condition
+        else if (false) return 0
+        else if (prop < 0x2000) target[prop & 0x07ff] = value
+        else if (prop < 0x4000) {
+          // here set content of PPU
+        }
+
+        return true
+      }
+    })
+
+    this.cpu.ram = proxyRAM
   }
 }
--- src/bus/index.test.js
+++ src/bus/index.test.js
@@ -1,4 +1,5 @@
 import Bus from './index'
+import CPU from '../6502/cpu'
 
 describe('Bus', () => {
   it('should be able to be connected to CPU', () => {
@@ -10,5 +11,17 @@ describe('Bus', () => {
 
     expect(bus.cpu).toBe(cpudummy)
     expect(cpudummy.connect).toHaveBeenCalledWith(bus)
+  })
+
+  describe('Proxy RAM', () => {
+    it('should mirror CPU RAM per 2KB', () => {
+      const cpu = new CPU([])
+      const bus = new Bus(cpu)
+
+      bus.cpu.ram[0x0001] = 0x12
+      expect(bus.cpu.ram[0x0801]).toBe(0x12)
+      expect(bus.cpu.ram[0x1001]).toBe(0x12)
+      expect(bus.cpu.ram[0x1801]).toBe(0x12)
+    })
   })
 })
