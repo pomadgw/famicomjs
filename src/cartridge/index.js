917367056a65b932b4af1936afb8892a4eee0120
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-30 14:55:08 +0700

    cartridge: add (dummy) method

--- src/cartridge/index.js
+++ src/cartridge/index.js
@@ -1,1 +1,16 @@
-export default class Cartridge {}
+export default class Cartridge {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {}
+
+  cpuRead(_addr) {
+    return 0
+  }
+
+  cpuWrite(_addr, _value) {}
+
+  ppuRead(_addr) {
+    return 0
+  }
+
+  ppuWrite(_addr, _value) {}
+}
