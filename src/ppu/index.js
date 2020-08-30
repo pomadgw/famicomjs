730be94f175af5593080196deb0688ea8dd448fc
Rahadian Yusuf <rahadian.y@gmail.com>
2020-08-30 14:54:55 +0700

    add comments on ppu

--- src/ppu/index.js
+++ src/ppu/index.js
@@ -2,11 +2,58 @@ export default class PPU {
   // eslint-disable-next-line no-useless-constructor
   constructor() {}
 
-  cpuRead(_addr) {
-    return 0
+  cpuRead(addr) {
+    // eslint-disable-next-line prefer-const
+    let data = 0
+
+    // TODO: implement this later
+    // switch (addr) {
+    //   case 0x0000: // Control
+    //     break
+    //   case 0x0001: // Mask
+    //     break
+    //   case 0x0002: // Status
+    //     break
+    //   case 0x0003: // OAM Address
+    //     break
+    //   case 0x0004: // OAM Data
+    //     break
+    //   case 0x0005: // Scroll
+    //     break
+    //   case 0x0006: // PPU Address
+    //     break
+    //   case 0x0007: // PPU Data
+    //     break
+    //   default:
+    //     break
+    // }
+
+    return data
   }
 
-  cpuWrite(_addr, _value) {}
+  cpuWrite(_addr, _value) {
+    // TODO: implement this later
+    // switch (addr) {
+    //   case 0x0000: // Control
+    //     break
+    //   case 0x0001: // Mask
+    //     break
+    //   case 0x0002: // Status
+    //     break
+    //   case 0x0003: // OAM Address
+    //     break
+    //   case 0x0004: // OAM Data
+    //     break
+    //   case 0x0005: // Scroll
+    //     break
+    //   case 0x0006: // PPU Address
+    //     break
+    //   case 0x0007: // PPU Data
+    //     break
+    //   default:
+    //     break
+    // }
+  }
 
   ppuRead(_addr) {
     return 0
