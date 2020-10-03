a6110492015d3c30f912b6702a61960c2341d1aa
Rahadian Yusuf <rahadian.y@gmail.com>
2020-10-03 16:51:39 +0700

    add file upload form for checking rom

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -1,1 +1,12 @@
+<script>
+  import Cartridge from '../cartridge'
+
+  function checkFile(event) {
+    const file = event.target.files[0]
+    console.log(file)
+    const cart = new Cartridge()
+    cart.parse(file)
+  }
+</script>
 <canvas id="canvas" width="640" height="480"></canvas>
+<input type="file" on:change={checkFile} />
