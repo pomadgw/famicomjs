e9de3882b4de287baa03c7f5f06635b31de63ccb
Rahadian Yusuf <rahadian.y@gmail.com>
2020-10-03 17:03:20 +0700

    have the cartridge veto on reading ram

--- src/bus/index.js
+++ src/bus/index.js
@@ -12,16 +12,14 @@ export default class Bus {
 
   initVRAM() {
     const cpuRAM = new Uint8Array(0x2000)
+    const thisBus = this
 
     const proxyRAM = new Proxy(cpuRAM, {
       get: (target, prop) => {
         const address = Number(prop)
         if (isNaN(address)) return target[prop]
 
-        // TODO:
-        // Add routine for fetching data from cartridge
-        // eslint-disable-next-line no-constant-condition
-        if (false) return 0
+        if (thisBus.cartridge.cpuRead(address)) return 0
         else if (address < 0x2000) return target[address & 0x07ff]
         else if (address < 0x4000) return this.ppu.cpuRead(address & 0x0007)
         return 0
@@ -29,10 +27,7 @@ export default class Bus {
       set: (target, prop, value) => {
         const address = Number(prop)
         if (isNaN(address)) target[prop] = value
-        // TODO:
-        // Add routine for fetching data writing status from cartridge
-        // eslint-disable-next-line no-constant-condition
-        else if (false) return 0
+        else if (thisBus.cartridge.cpuWrite(address, value)) return true
         else if (address < 0x2000) target[prop & 0x07ff] = value
         else if (address < 0x4000) {
           this.ppu.cpuWrite(address & 0x0007, value)
--- src/bus/index.test.js
+++ src/bus/index.test.js
@@ -2,6 +2,13 @@ import Bus from './index'
 import CPU from '../6502/cpu'
 import PPU from '../ppu'
 
+const createDummyCartridge = (defaultValue = false) => ({
+  cpuRead: () => defaultValue,
+  cpuWrite: () => defaultValue,
+  ppuRead: () => defaultValue,
+  ppuWrite: () => defaultValue
+})
+
 describe('Bus', () => {
   it('should be able to be connected to CPU', () => {
     const cpudummy = {
@@ -17,7 +24,9 @@ describe('Bus', () => {
   describe('Proxy RAM', () => {
     it('should mirror CPU RAM per 2KB', () => {
       const cpu = new CPU([])
-      const bus = new Bus(cpu)
+      const ppu = new PPU()
+      const bus = new Bus(cpu, ppu)
+      bus.insertCartridge(createDummyCartridge(false))
 
       bus.ram[0x0001] = 0x12
       expect(bus.ram[0x0801]).toBe(0x12)
@@ -25,10 +34,24 @@ describe('Bus', () => {
       expect(bus.ram[0x1801]).toBe(0x12)
     })
 
+    it('should cancel reading data if cartridge is handling it', () => {
+      const cpu = new CPU([])
+      const ppu = new PPU()
+      const bus = new Bus(cpu, ppu)
+      bus.insertCartridge(createDummyCartridge(true))
+
+      jest.spyOn(ppu, 'cpuRead')
+
+      const data = bus.ram[0x2001]
+      expect(data).toBe(0)
+      expect(ppu.cpuRead).not.toHaveBeenCalled()
+    })
+
     it('should read data from PPU if address is 0x2000 - 0x3fff', () => {
       const cpu = new CPU([])
       const ppu = new PPU()
       const bus = new Bus(cpu, ppu)
+      bus.insertCartridge(createDummyCartridge(false))
 
       jest.spyOn(ppu, 'cpuRead')
 
@@ -41,6 +64,7 @@ describe('Bus', () => {
       const cpu = new CPU([])
       const ppu = new PPU()
       const bus = new Bus(cpu, ppu)
+      bus.insertCartridge(createDummyCartridge(false))
 
       jest.spyOn(ppu, 'cpuWrite')
 
