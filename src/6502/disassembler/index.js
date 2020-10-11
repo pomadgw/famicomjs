6243f86261dce47be68764c145f3b1d19a03b23d
Rahadian Yusuf <rahadian.y@gmail.com>
2020-10-11 21:25:01 +0700

    show ram content

--- src/6502/disassembler/index.js
+++ src/6502/disassembler/index.js
@@ -8,73 +8,75 @@ function toHex(number, padding = 2) {
   return `$${number.toString(16).toUpperCase().padStart(padding, '0')}`
 }
 
-export default function disassemble(codes = [], { binaryStart } = {}) {
-  const argParams = {
-    ABS: {
-      length: 2,
-      stringify: (name, params) =>
-        `${name} ${toHex(twoUint8ToUint16(...params), 4)}`
-    },
-    ABX: {
-      length: 2,
-      stringify: (name, params) =>
-        `${name} ${toHex(twoUint8ToUint16(...params), 4)},X`
-    },
-    ABY: {
-      length: 2,
-      stringify: (name, params) =>
-        `${name} ${toHex(twoUint8ToUint16(...params), 4)},Y`
-    },
-    IMM: {
-      length: 1,
-      stringify: (name, params) => `${name} #${toHex(params[0])}`
-    },
-    IMP: {
-      length: 0,
-      stringify: (name, params) => {
-        if (name === 'ASL') return 'ASL A'
-        return name
-      }
-    },
-    IND: {
-      length: 2,
-      stringify: (name, params) =>
-        `${name} (${toHex(twoUint8ToUint16(...params), 4)})`
-    },
-    IZX: {
-      length: 1,
-      stringify: (name, params) => `${name} (${toHex(params[0])},X)`
-    },
-    IZY: {
-      length: 1,
-      stringify: (name, params) => `${name} (${toHex(params[0])}),Y`
-    },
-    REL: {
-      length: 1,
-      stringify: (name, params, line) => {
-        let result = `${name} ${toHex(params[0])}`
-
-        if (binaryStart) {
-          const offset = new Int8Array([params[0]])[0]
-          result = `${result} // [${toHex(line + offset + 2, 4)}]`
-        }
-
-        return result
-      }
-    },
-    ZP0: {
-      length: 1,
-      stringify: (name, params) => `${name} ${toHex(params[0])}`
-    },
-    ZPX: {
-      length: 1,
-      stringify: (name, params) => `${name} ${toHex(params[0])},X`
-    },
-    ZPY: {
-      length: 1,
-      stringify: (name, params) => `${name} ${toHex(params[0])},Y`
+export const argParamsGenerator = (binaryStart) => ({
+  ABS: {
+    length: 2,
+    stringify: (name, params) =>
+      `${name} ${toHex(twoUint8ToUint16(...params), 4)}`
+  },
+  ABX: {
+    length: 2,
+    stringify: (name, params) =>
+      `${name} ${toHex(twoUint8ToUint16(...params), 4)},X`
+  },
+  ABY: {
+    length: 2,
+    stringify: (name, params) =>
+      `${name} ${toHex(twoUint8ToUint16(...params), 4)},Y`
+  },
+  IMM: {
+    length: 1,
+    stringify: (name, params) => `${name} #${toHex(params[0])}`
+  },
+  IMP: {
+    length: 0,
+    stringify: (name, params) => {
+      if (name === 'ASL') return 'ASL A'
+      return name
     }
+  },
+  IND: {
+    length: 2,
+    stringify: (name, params) =>
+      `${name} (${toHex(twoUint8ToUint16(...params), 4)})`
+  },
+  IZX: {
+    length: 1,
+    stringify: (name, params) => `${name} (${toHex(params[0])},X)`
+  },
+  IZY: {
+    length: 1,
+    stringify: (name, params) => `${name} (${toHex(params[0])}),Y`
+  },
+  REL: {
+    length: 1,
+    stringify: (name, params, line) => {
+      let result = `${name} ${toHex(params[0])}`
+
+      if (binaryStart) {
+        const offset = new Int8Array([params[0]])[0]
+        result = `${result} // [${toHex(line + offset + 2, 4)}]`
+      }
+
+      return result
+    }
+  },
+  ZP0: {
+    length: 1,
+    stringify: (name, params) => `${name} ${toHex(params[0])}`
+  },
+  ZPX: {
+    length: 1,
+    stringify: (name, params) => `${name} ${toHex(params[0])},X`
+  },
+  ZPY: {
+    length: 1,
+    stringify: (name, params) => `${name} ${toHex(params[0])},Y`
   }
+})
+
+export default function disassemble(codes = [], { binaryStart } = {}) {
+  const argParams = argParamsGenerator(binaryStart)
 
   const result = {}
   const copyOfCode = []
--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -13,6 +13,8 @@
   let emulationMode = false
   let ctx
   let startFrame
+  let showRAM = true
+  let offsetStart = 0x8000
 
   nes = new Bus(new CPU(), new PPU())
 
@@ -28,10 +30,13 @@
     nes.cartridge = cart
     nes.insertCartridge(cart)
     nes.reset()
+
+    offsetStart = nes.cpu.registers.PC
   }
 
   function resetNES() {
     nes.reset()
+    offsetStart = nes.cpu.registers.PC
   }
 
   function stepNES() {
@@ -42,6 +47,8 @@
     do {
       nes.clock()
     } while (nes.cpu.isComplete)
+
+    offsetStart = nes.cpu.registers.PC
 
     render()
   }
@@ -68,6 +75,8 @@
     } while (nes.cpu.isComplete)
     nes.ppu.isFrameComplete = false
 
+    offsetStart = nes.cpu.registers.PC
+
     render()
   }
 
@@ -82,6 +91,8 @@
           nes.clock()
         } while (!nes.ppu.isFrameComplete)
         nes.ppu.isFrameComplete = false
+
+        offsetStart = nes.cpu.registers.PC
 
         render()
       }
@@ -100,13 +111,33 @@
   })
 </script>
 
-<canvas style="display: none" width="640" height="480" bind:this={canvas}></canvas>
-<canvas width="640" height="480" bind:this={zoomCanvas}></canvas>
-<input type="file" on:change={readFile} />
-<button on:click={resetNES}>Reset</button>
-<button on:click={stepNES}>Execute Code Step-by-Step</button>
-<button on:click={renderSingleFrame}>Execute Code for Whole Frame</button>
-<button on:click={toggleEmulation}>Toggle Emulation: {emulationMode ? 'on' : 'off'}</button>
-{#if nes.cartridge}
-<RAM ram={nes.cpu.ram} offsetStart={0x8000} offsetEnd={0x80ff} />
-{/if}
+<div class="flex m-auto p-10">
+  <div class="flex flex-col flex-2">
+    <div>
+      <canvas class="hidden" width="256" height="240" bind:this={canvas}></canvas>
+      <canvas width="640" height="480" bind:this={zoomCanvas}></canvas>
+    </div>
+    <div class="flex items-center mt-4">
+      <input type="file" on:change={readFile} />
+      <label>
+        <input type="checkbox" bind:checked={showRAM} />
+        Show RAM content
+      </label>
+      <button class="ml-2" on:click={resetNES}>Reset</button>
+      <button class="ml-2" on:click={stepNES}>Execute Code Step-by-Step</button>
+      <button class="ml-2" on:click={renderSingleFrame}>Execute Code for Whole Frame</button>
+      <button class="ml-2" on:click={toggleEmulation}>Toggle Emulation: {emulationMode ? 'on' : 'off'}</button>
+    </div>
+  </div>
+  <div class="ml-4 flex-1">
+    <div>
+      PC: <span class="font-mono">${offsetStart.toString(16).padStart(4, '0')}</span>
+    </div>
+    {#if nes.cartridge && showRAM}
+    <div class="mt-4">
+      <RAM ram={nes.cpu.ram} offsetStart={offsetStart} offsetEnd={offsetStart + 0x30} />
+    </div>
+    {/if}
+
+  </div>
+</div>
