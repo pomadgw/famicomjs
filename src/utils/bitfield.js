21a58acfb123ce629016aef2f56b88c6fb6c8f20
Rahadian Yusuf <rahadian.y@gmail.com>
2020-10-12 22:38:13 +0700

    add bitfield

---
+++ src/utils/bitfield.js
@@ -0,0 +1,41 @@
+export default function bitfield(structure, arrayedNumber) {
+  const result = {}
+  const totalSize = structure.map((e) => e[1]).reduce((a, b) => a + b, 0)
+
+  if (totalSize > arrayedNumber.BYTES_PER_ELEMENT * 8) {
+    throw new Error(
+      `Expected max ${
+        arrayedNumber.BYTES_PER_ELEMENT * 8
+      } bits, got ${totalSize}`
+    )
+  }
+
+  Object.defineProperty(result, 'value', {
+    get() {
+      return arrayedNumber[0]
+    },
+    set(value) {
+      arrayedNumber[0] = value
+    }
+  })
+
+  let pos = 0
+  structure.forEach(([field, size]) => {
+    const posStart = pos
+
+    Object.defineProperty(result, field, {
+      get() {
+        return (arrayedNumber[0] >> posStart) & ((1 << size) - 1)
+      },
+      set(value) {
+        value = value & ((1 << size) - 1)
+        value = value << posStart
+        arrayedNumber[0] = arrayedNumber[0] | value
+      }
+    })
+
+    pos += size
+  })
+
+  return result
+}
---
+++ src/utils/bitfield.test.js
@@ -0,0 +1,46 @@
+import bitfield from './bitfield'
+
+describe('bitfield', () => {
+  it('should be able to change a value', () => {
+    const field = bitfield([], new Uint8Array([0]))
+    expect(field.value).toBe(0)
+
+    field.value = 10
+    expect(field.value).toBe(10)
+  })
+
+  it('should unmatched size', () => {
+    expect(() =>
+      bitfield(
+        [
+          ['dummy', 5],
+          ['dummy2', 4]
+        ],
+        new Uint8Array([0])
+      )
+    ).toThrowError('Expected max 8 bits, got 9')
+  })
+
+  it('should be able to change a bitfield', () => {
+    const field = bitfield(
+      [
+        ['data', 4],
+        ['data2', 4],
+        ['data3', 5],
+        ['data4', 3]
+      ],
+      new Uint16Array([0])
+    )
+    expect(field.data).toBe(0)
+
+    field.data = 0x07
+    field.data2 = 0x01
+    field.data3 = 0x1d
+    field.data4 = 0x03
+    expect(field.data).toBe(0x07)
+    expect(field.data2).toBe(0x01)
+    expect(field.data3).toBe(0x1d)
+    expect(field.data4).toBe(0x03)
+    expect(field.value).toBe((0x03 << 13) | (0x1d << 8) | (0x01 << 4) | 0x07)
+  })
+})
