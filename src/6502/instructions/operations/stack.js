126e387f6d2a9afa1b60b7c50c3b44bdbfb8c1b5
Rahadian Yusuf <rahadian.y@gmail.com>
2020-10-15 06:14:27 +0700

    "standardize" way to check negative

--- src/6502/instructions/operations/stack.js
+++ src/6502/instructions/operations/stack.js
@@ -7,7 +7,7 @@ export function PHA(cpu) {
 
 export function PLA(cpu) {
   cpu.registers.A = cpu.popStack()
-  cpu.registers.STATUS.N = cpu.registers.A >= 0x80
+  cpu.registers.STATUS.N = (cpu.registers.A & 0x80) > 0
   cpu.registers.STATUS.Z = cpu.registers.A === 0
   return 0
 }
@@ -36,7 +36,7 @@ export function TXS(cpu) {
 
 export function TSX(cpu) {
   cpu.registers.X = cpu.registers.SP
-  cpu.registers.STATUS.N = cpu.registers.X >= 0x80
+  cpu.registers.STATUS.N = (cpu.registers.X & 0x80) > 0
   cpu.registers.STATUS.Z = cpu.registers.X === 0
   return 0
 }
