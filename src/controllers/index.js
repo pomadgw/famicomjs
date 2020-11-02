6fc38df6c541d46814c53146134f90c9cc9b0393
Rahadian Yusuf <rahadian.y@gmail.com>
2020-11-03 06:46:25 +0700

    add controller

---
+++ src/controllers/index.js
@@ -0,0 +1,48 @@
+export const Button = {
+  A: 1 << 0,
+  B: 1 << 1,
+  Select: 1 << 2,
+  Start: 1 << 3,
+  Up: 1 << 4,
+  Down: 1 << 5,
+  Left: 1 << 6,
+  Right: 1 << 7
+}
+
+export default class Controller {
+  constructor(buttons) {
+    this.buttonStatus = 0
+    this.buttonStatusToSend = 0
+    this.strobe = false
+  }
+
+  write(value) {
+    this.strobe = (value & 1) > 0
+  }
+
+  read() {
+    if (this.strobe) {
+      return this.buttonStatus & 1
+    } else if (this.buttonStatusToSend === 0) {
+      return 1
+    } else {
+      const value = this.buttonStatusToSend & 1
+      this.buttonStatusToSend >>= 1
+      return value
+    }
+  }
+
+  setButtonState(buttonType, value) {
+    const buttonValue = Button[buttonType]
+
+    if (value) {
+      this.buttonStatus |= buttonValue
+    } else {
+      this.buttonStatus &= ~buttonValue
+    }
+
+    if (this.strobe) {
+      this.buttonStatusToSend = this.buttonStatus
+    }
+  }
+}
