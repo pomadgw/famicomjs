7211e27e039b601f4ad98be5fef4d594656c16eb
Rahadian Yusuf <rahadian.y@gmail.com>
2020-11-07 00:11:54 +0700

    remove example export

--- src/assembly/index.ts
+++ src/assembly/index.ts
@@ -2,11 +2,6 @@
 import CPU from './cpu'
 import Bus from './bus'
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-
 export default function createNES(): Bus {
   const bus = new Bus()
   const cpu = new CPU(bus)
