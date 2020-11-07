7116b9b0ea95145c912f89c342463c8dc0ba09d5
Rahadian Yusuf <rahadian.y@gmail.com>
2020-11-07 11:06:41 +0700

    use createNES in test

--- src/assembly/cpu/addressings/absolute.test.js
+++ src/assembly/cpu/addressings/absolute.test.js
@@ -1,49 +1,45 @@
-import Bus from '../../bus'
-import CPU from '../../cpu'
+import createNES from '../../index'
 
 describe('6502 CPU (wasm): addressing mode: absolute', () => {
   it('should return correct value for absolute address', () => {
-    const bus = new Bus()
-    const CPUDummy = new CPU(bus)
+    const bus = createNES()
     bus.ram = new Uint8Array([0x00, 0x0f, 0])
-    CPUDummy.absMode()
-    expect(CPUDummy).toEqual(
+    bus.cpu.absMode()
+    expect(bus.cpu).toEqual(
       expect.objectContaining({ absoluteAddress: 0x0f00, clocks: 0 })
     )
   })
 
   it('should return correct value for indexed absolute address', () => {
-    const bus = new Bus()
-    const CPUDummy = new CPU(bus)
+    const bus = createNES()
     bus.ram = new Uint8Array([0x00, 0x0f, 0])
 
-    CPUDummy.X = 0x01
-    CPUDummy.absXMode()
-    expect(CPUDummy).toEqual(
+    bus.cpu.X = 0x01
+    bus.cpu.absXMode()
+    expect(bus.cpu).toEqual(
       expect.objectContaining({ absoluteAddress: 0x0f01, clocks: 0 })
     )
 
-    CPUDummy.PC = 0
-    CPUDummy.Y = 0x02
-    CPUDummy.absYMode()
-    expect(CPUDummy).toEqual(
+    bus.cpu.PC = 0
+    bus.cpu.Y = 0x02
+    bus.cpu.absYMode()
+    expect(bus.cpu).toEqual(
       expect.objectContaining({ absoluteAddress: 0x0f02, clocks: 0 })
     )
   })
 
   it('should return additional clock for indexed absolute address if page is changed', () => {
-    const bus = new Bus()
-    const CPUDummy = new CPU(bus)
+    const bus = createNES()
     bus.ram = new Uint8Array([0xff, 0x0f, 0])
 
-    CPUDummy.X = 0x01
-    CPUDummy.absXMode()
-    expect(CPUDummy.clocks).toEqual(1)
+    bus.cpu.X = 0x01
+    bus.cpu.absXMode()
+    expect(bus.cpu.clocks).toEqual(1)
 
-    CPUDummy.PC = 0
-    CPUDummy.clocks = 0
-    CPUDummy.Y = 0x02
-    CPUDummy.absYMode()
-    expect(CPUDummy.clocks).toEqual(1)
+    bus.cpu.PC = 0
+    bus.cpu.clocks = 0
+    bus.cpu.Y = 0x02
+    bus.cpu.absYMode()
+    expect(bus.cpu.clocks).toEqual(1)
   })
 })
