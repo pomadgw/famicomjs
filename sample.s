5ff962dab0f3e0cdbe32c70e061d63dd24041c5a
Rahadian Yusuf <rahadian.y@gmail.com>
2020-11-12 21:43:03 +0700

    update sample.s

--- sample.s
+++ sample.s
@@ -1,2 +1,18 @@
-LDA #$F0
-STA $1000
+.data $05D0 "Hello, world"
+
+.start $0600
+      JSR init
+      JSR loop
+      JSR end
+
+init: LDX #$00
+      RTS
+
+loop: INX
+      CPX #$05
+      BNE loop
+      RTS
+
+end:  BRK
+
+.reset $0600
