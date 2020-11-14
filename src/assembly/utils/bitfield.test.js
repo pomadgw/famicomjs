d6b2bcb6c0f09f574231baffd459df98c786d735
Rahadian Yusuf <rahadian.y@gmail.com>
2020-11-14 18:13:31 +0700

    implement bitfield

--- src/assembly/utils/bitfield.test.js
+++ src/assembly/utils/bitfield.test.js
@@ -1,6 +1,6 @@
 import bitfield from './bitfield'
 
-describe.skip('bitfield', () => {
+describe('bitfield', () => {
   it('should be able to change a value', () => {
     const field = new bitfield([], new Uint8Array([0]))
     expect(field.value).toBe(0)
@@ -9,74 +9,86 @@ describe.skip('bitfield', () => {
     expect(field.value).toBe(10)
   })
 
-  // it('should unmatched size', () => {
-  //   expect(() =>
-  //     bitfield(
-  //       [
-  //         ['dummy', 5],
-  //         ['dummy2', 4]
-  //       ],
-  //       new Uint8Array([0])
-  //     )
-  //   ).toThrowError('Expected max 8 bits, got 9')
-  // })
-
   it('should be able to change a bitfield', () => {
-    const field = bitfield(
+    const field = new bitfield(
       [
-        ['data', 4],
-        ['data2', 4],
-        ['data3', 5],
-        ['data4', 3]
+        {
+          name: 'data',
+          size: 4
+        },
+        {
+          name: 'data2',
+          size: 4
+        },
+        {
+          name: 'data3',
+          size: 5
+        },
+        {
+          name: 'data4',
+          size: 5
+        }
       ],
-      new Uint16Array([0])
+      new Uint32Array([0])
     )
-    expect(field.data).toBe(0)
+    expect(field.get('data')).toBe(0)
 
-    field.data = 0x07
-    field.data2 = 0x00
-    field.data3 = 0x1d
-    field.data4 = 0x03
-    expect(field.data).toBe(0x07)
-    expect(field.data2).toBe(0x00)
-    expect(field.data3).toBe(0x1d)
-    expect(field.data4).toBe(0x03)
+    field.set('data', 0x07)
+    field.set('data2', 0x00)
+    field.set('data3', 0x1d)
+    field.set('data4', 0x03)
+    expect(field.get('data')).toBe(0x07)
+    expect(field.get('data2')).toBe(0x00)
+    expect(field.get('data3')).toBe(0x1d)
+    expect(field.get('data4')).toBe(0x03)
     expect(field.value).toBe((0x03 << 13) | (0x1d << 8) | (0x00 << 4) | 0x07)
 
-    field.data4 = 0x00
-    expect(field.data4).toBe(0x00)
+    field.set('data4', 0x00)
+    expect(field.get('data4')).toBe(0x00)
 
-    const loopy = bitfield(
+    const loopy = new bitfield(
       [
         ['coarseX', 5],
         ['coarseY', 5],
         ['nametableX', 1],
         ['nametableY', 1],
         ['fineY', 3],
-        ['unused', 1]
+        ['unused', 1],
+        { name: 'coarseX', size: 5 },
+        { name: 'coarseY', size: 5 },
+        { name: 'nametableX', size: 1 },
+        { name: 'nametableY', size: 1 },
+        { name: 'fineY', size: 3 },
+        { name: 'unused', size: 1 }
       ],
-      new Uint16Array([0])
+      new Uint32Array([0])
     )
 
     loopy.value = 0
-    loopy.coarseX = 0x17 >> 3
-    loopy.fineY = 7
-    loopy.coarseY = 0x17 >> 3
-    expect(loopy.fineY).toBe(7)
+    loopy.set('coarseX', 0x17 >> 3)
+    loopy.set('fineY', 0x17 >> 3)
+    loopy.set('fineY', 7)
+    expect(loopy.get('fineY')).toBe(7)
   })
 
   it('should be able to change a bitfield as a boolean', () => {
-    const field = bitfield(
+    const field = new bitfield(
       [
-        ['data', 1],
-        ['data2', 4]
+        {
+          name: 'data',
+          size: 1
+        },
+        {
+          name: 'data2',
+          size: 4
+        }
       ],
-      new Uint8Array([0])
+      new Uint32Array([0])
     )
 
-    expect(field.bData).toBe(false)
-    field.bData = true
-    expect(field.bData).toBe(true)
-    expect(field.data).toBe(1)
+    expect(field.getAsBoolean('data')).toBe(false)
+    field.setAsBoolean('data', true)
+    expect(field.getAsBoolean('data')).toBe(true)
+    expect(field.get('data')).toBe(1)
   })
 })
--- src/assembly/utils/bitfield.ts
+++ src/assembly/utils/bitfield.ts
@@ -1,13 +1,39 @@
 class BitfieldSize {
   public name: string
   public size: u8
+
+  constructor() {
+    this.size = 0
+    this.name = ''
+  }
 }
 
-export default class Bitfield {
+class BitfieldSizeWihStartPos extends BitfieldSize {
+  public startPos: u8
+}
+
+export default class BitfieldTemplate {
   private _value: Uint32Array
+  private _propsMap: Map<string, BitfieldSizeWihStartPos>
 
   constructor(props: BitfieldSize[], value: Uint32Array) {
     this._value = value
+
+    this._propsMap = new Map<string, BitfieldSizeWihStartPos>()
+
+    let pos = 0
+    for (let idx = 0; idx < props.length; idx++) {
+      const prop = props[idx]
+      const startPos = pos as u8
+      const newProp: BitfieldSizeWihStartPos = {
+        name: prop.name,
+        size: prop.size,
+        startPos
+      }
+
+      this._propsMap.set(prop.name, newProp)
+      pos += prop.size
+    }
   }
 
   get value(): u32 {
@@ -16,5 +42,28 @@ export default class Bitfield {
 
   set value(value: u32) {
     this._value[0] = value
+  }
+
+  get(name: string): u32 {
+    const prop = this._propsMap.get(name)
+    return (this.value >> prop.startPos) & ((1 << prop.size) - 1)
+  }
+
+  getAsBoolean(name: string): bool {
+    return this.get(name) === 1
+  }
+
+  set(name: string, value: u32): void {
+    const prop = this._propsMap.get(name)
+    const resetBits = (1 << prop.size) - 1
+    const reset = ~(resetBits << prop.startPos)
+
+    value = value & resetBits
+    value = value << prop.startPos
+    this.value = (this.value & reset) | value
+  }
+
+  setAsBoolean(name: string, value: bool): void {
+    this.set(name, value ? 1 : 0)
   }
 }
