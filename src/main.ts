9d8c59ede04513001b203deb87734f0e118ae52c
Rahadian Yusuf <rahadian.y@gmail.com>
2021-02-28 14:51:51 +0700

    cpu: test on browser

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,17 @@ import wasm, { NES } from '../nes/pkg/nes'
 
 wasm().then((e) => {
   const nes = NES.new()
+  nes.write(0xfffc, 0x12)
+  nes.write(0xfffd, 0x34)
+  console.log(nes.read(0xfffc))
+  console.log(nes.read(0xfffd))
+  nes.reset()
+
   nes.clock()
+  while (!nes.cpu.sync) {
+    nes.clock()
+  }
   console.log(e.get_nes_screen_buffer_pointer())
+  console.log(nes.cpu.cycles)
+  console.log(nes.cpu.pc)
 })
