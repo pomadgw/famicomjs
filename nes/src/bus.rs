2720cd5d62d72efa37512e313540a53d3a806cbe
Rahadian Yusuf <rahadian.y@gmail.com>
2021-05-22 13:07:11 +0700

    cleanup

--- nes/src/bus.rs
+++ nes/src/bus.rs
@@ -1,16 +1,13 @@
-use crate::ppu::PPU;
 use nesrs::memory::Memory;
 
 pub struct Bus {
     ram: Vec<u8>,
-    ppu: PPU,
 }
 
 impl Bus {
     pub fn new() -> Bus {
         Bus {
             ram: vec![0; 0x10000],
-            ppu: PPU::new(),
         }
     }
 }
@@ -24,14 +21,3 @@ impl Memory for Bus {
         self.ram[address] = value;
     }
 }
-
-// // #[wasm_bindgen]
-// impl IO for Bus {
-//   fn read(&self, address: u16) -> u8 {
-//     self.ram[address as usize]
-//   }
-
-//   fn write(&mut self, address: u16, value: u8) {
-//     self.ram[address as usize] = value;
-//   }
-// }
