f9aee2abd34ffc4dbf42c82f66bd32e3e24a1864
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-24 21:54:25 +0700

    update nesrs lib

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,27 @@ import wasm, { NES } from '../nes/pkg/nes'
 
 wasm().then((e) => {
   const nes = NES.new()
-  nes.write(0xfffc, 0x12)
-  nes.write(0xfffd, 0x34)
+  nes.write(0xfffc, 0x00)
+  nes.write(0xfffd, 0x80)
   console.log(nes.read(0xfffc))
   console.log(nes.read(0xfffd))
   nes.reset()
 
-  nes.clock()
-  while (!nes.is_cpu_done()) {
+  nes.write(0x8000, 0xa9)
+  nes.write(0x8001, 0x10)
+  nes.write(0x8002, 0xaa)
+
+  const clock = () => {
     nes.clock()
+    while (!nes.is_cpu_done()) {
+      nes.clock()
+    }
+    console.log(nes.debug())
   }
+
+  clock()
+  clock()
+  clock()
+
   console.log(e.get_nes_screen_buffer_pointer())
-  console.log(nes.debug())
 })
