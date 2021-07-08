88b2b0cb8c5b10026ab2b0cd62c793a23e71a2e6
Rahadian Yusuf <rahadian.y@gmail.com>
2021-07-08 10:13:44 +0700

    PPU (#38)
    * try the lib
    
    * update neslib and use ssh
    
    * remove whitespace
    
    * optimize build-rs
    
    * use ppu branch
    
    * fix
    
    * readd submodule
    
    * move screen buffer
    
    * render canvas
    
    * try render to canvas
    
    * update module
    
    * fix pointer
    
    * update
    
    * update
    
    * update library
    
    * render NES's bg
    
    * eslint
    
    * format
    
    * install
    
    * use yarn.lock

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,4 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import wasm, { NES } from '../nes/pkg/nes'
-
-// createApp(App).mount('#app')
-
-wasm().then((e) => {
-  const nes = NES.new()
-  nes.write(0xfffc, 0x00)
-  nes.write(0xfffd, 0x80)
-  console.log(nes.read(0xfffc))
-  console.log(nes.read(0xfffd))
-  nes.reset()
-
-  nes.write(0x8000, 0xa9)
-  nes.write(0x8001, 0x10)
-  nes.write(0x8002, 0xaa)
-
-  const clock = () => {
-    nes.clock()
-    while (!nes.is_cpu_done()) {
-      nes.clock()
-    }
-    console.log(nes.debug())
-  }
-
-  clock()
-  clock()
-  clock()
-
-  console.log(e.get_nes_screen_buffer_pointer())
-})
+createApp(App).mount('#app')
